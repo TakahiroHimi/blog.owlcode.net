cf7c7b3d0d3a35d69cfce609330061d24c24e892
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-08-21 16:55:29 +0900

    fix: フォント変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
             rel="stylesheet"
           />
         </Head>
