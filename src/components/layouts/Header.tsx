a33053941f4cf4bbf97ddaf0e6cc87ca18889bd8
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-10-12 23:46:46 +0900

    fix: ヘッダー修正

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,21 @@
 import { css } from '@emotion/react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React, { VFC } from 'react'
 import colors from 'styles/colors'
+import owl from './Header.owl.png'
 
 const Header: VFC = () => {
   return (
     <header>
       <Link href="/">
         <a css={link}>
-          <h1 css={h1}>{process.env.NEXT_PUBLIC_BLOG_TITLE}</h1>
+          <div css={container}>
+            <h1 css={h1}>{process.env.NEXT_PUBLIC_BLOG_TITLE}</h1>
+            <div css={imageWrapper}>
+              <Image src={owl} alt="owl" />
+            </div>
+          </div>
         </a>
       </Link>
     </header>
@@ -22,12 +29,27 @@ const link = css`
   cursor: pointer;
 `
 
+const container = css`
+  background-color: ${colors.blue400}CC;
+  position: relative;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const h1 = css`
   color: ${colors.white};
   text-align: center;
-  padding: 64px 0px;
-  background-image: linear-gradient(to top, ${colors.blue50} 0%, ${colors.blue200} 100%);
-  font-size: 36px;
+  font-size: 32px;
   font-weight: bold;
   font-family: 'Roboto', 'Noto Sans JP', sans-serif;
+`
+
+const imageWrapper = css`
+  width: 180px;
+  position: absolute;
+  transform: translate(0px, -15px);
+  top: 0;
+  right: 0;
 `
